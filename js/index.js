2eb82a00ebf707ea1ec57a1552642ca937888c3f
maazk <maazkb12@gmail.com>
2026-03-09 04:27:43 +0500

    Personalized for Women's Day

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `to the woman i love ragebaiting`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
